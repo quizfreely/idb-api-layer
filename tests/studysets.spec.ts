7ad9b6fa03a7bf4c21f4d64c46399955bd8da2a3
Ehan A <ehan@quizfreely.org>
2026-03-17 18:05:29 -0400

    fix test

--- tests/studysets.spec.ts
+++ tests/studysets.spec.ts
@@ -3,31 +3,39 @@ import { test, expect } from '@playwright/test';
 test('create, view, update, & delete studyset', async ({ page }) => {
   await page.goto('/');
 
-  await page.evaluate(async () => {
+  const result = await page.evaluate(async () => {
     const id = await window.idbApiLayer.createStudyset({
       title: "",
       draft: true
     });
+
     let studyset = await window.idbApiLayer.getStudysetById(id);
-    expect(studyset?.title).toEqual("");
-    expect(studyset?.draft).toStrictEqual(true);
 
     await window.idbApiLayer.updateStudyset({
-      id: id,
+      id,
       title: "",
       draft: false
     });
-    studyset = await window.idbApiLayer.getStudysetById(id);
-    expect(studyset?.title).toEqual("Untitled Studyset");
-    expect(studyset?.draft).toStrictEqual(false);
+
+    let updated1 = await window.idbApiLayer.getStudysetById(id);
 
     await window.idbApiLayer.updateStudyset({
-      id: id,
+      id,
       title: "New Title!!!",
       draft: false
     });
-    studyset = await window.idbApiLayer.getStudysetById(id);
-    expect(studyset?.title).toEqual("New Title!!!");
-    expect(studyset?.draft).toStrictEqual(false);
-  })
+
+    let updated2 = await window.idbApiLayer.getStudysetById(id);
+
+    return { studyset, updated1, updated2 };
+  });
+
+  expect(result.studyset?.title).toBe("");
+  expect(result.studyset?.draft).toBe(true);
+
+  expect(result.updated1?.title).toBe("Untitled Studyset");
+  expect(result.updated1?.draft).toBe(false);
+
+  expect(result.updated2?.title).toBe("New Title!!!");
+  expect(result.updated2?.draft).toBe(false);
 });
